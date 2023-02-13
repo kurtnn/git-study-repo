d1d44bbddb1585f0f526a12efab1fcdd5b2f2858
kurtnn <97244726+kurtnn@users.noreply.github.com>
2023-02-13 09:10:27 +0300

    feature 02 completed
    * Commit 1 for f02
    
    * Commit 2 for f02
    
    * Commit 3 for f02

--- src/AClass.java
+++ src/AClass.java
@@ -1,3 +1,3 @@
 public class AClass {
-    private String val = "Commit 1 for f01";
+    private String val = "Commit 3 for f02";
 }
--- src/BClass.java
+++ src/BClass.java
@@ -1,3 +1,3 @@
 public class BClass {
-    private String val = "Commit 1 for f01";
+    private String val = "Commit 2 for f02";
 }
