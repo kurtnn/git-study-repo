234afde5ec7a125daa260f57207603f70a904868
kurtnn <kurtnurcan@gmail.com>
2023-02-15 11:23:20 +0300

    f2 Can c2 + c3 commitler

--- src/AClass.java
+++ src/AClass.java
@@ -1,4 +1,4 @@
 public class AClass {
     private String val = "f1 Ahmet c1";
-    private String val2 = "f2 Can c1";
+    private String val2 = "f2 Can c2";
 }
--- src/BClass.java
+++ src/BClass.java
@@ -1,4 +1,4 @@
 public class BClass {
-    private String val = "f2 Can c1";
+    private String val = "f2 Can c3";
     private String val2 = "f1 Ahmet c2";
 }
