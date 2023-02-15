5932b7967c41f8744046fa6957c5d5b73642bd67
kurtnn <kurtnurcan@gmail.com>
2023-02-15 10:06:17 +0300

    Revert "f2 Can c1"
    This reverts commit 434b2801142b4d47d03fa9719555b98d2749be7d.

--- src/AClass.java
+++ src/AClass.java
@@ -1,3 +1,3 @@
 public class AClass {
-    private String val = "f2 Can c1";
+    private String val = "Commit 1 for f1";
 }
--- src/BClass.java
+++ src/BClass.java
@@ -1,3 +1,3 @@
 public class BClass {
-    private String val = "f2 Can c1";
+    private String val = "Commit 2 for f-102";
 }
